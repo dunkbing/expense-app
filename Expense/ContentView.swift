3cb6d2e0e79da6d10a767fa67bf6939244e55ce6
Binh.BuiDang <dangbinh4869@gmail.com>
2024-08-07 09:40:57 +0700

    Initial Commit

---
+++ Expense/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Expense
+//
+//  Created by Bing on 7/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Expense/ExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseApp.swift
+//  Expense
+//
+//  Created by Bing on 7/8/24.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
